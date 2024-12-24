b43fb888b99355a341703b2abc19d44ba37f830a
Krusovice <jmk1589@gmail.com>
2024-12-24 08:24:09 +0100

    Working with the ml model

--- foundationResponse/ML/linearRegression.py
+++ foundationResponse/ML/linearRegression.py
@@ -1,10 +1,11 @@
 import numpy as np
 import pandas as pd
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression, Lasso, ElasticNet, LassoCV
+from sklearn.preprocessing import PolynomialFeatures
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
 
-filePath = r"C:\Users\jmkir\Ramboll\JMKIR - Documents\10. projects\94. ML, Rocking foundation\dataFile.json"
+filePath = "dataFile.json"
 df = pd.read_json(filePath)
 
 # Substituting no soil values with a hard soil placeholder value
@@ -23,12 +24,27 @@ soils_df = pd.DataFrame(df['soilsNew'].to_list(), columns=[f'soil_layer_{i}' for
 df = pd.concat([df, soils_df], axis=1)
 df = df.drop(columns=['soils','soilsNew'])
 
+# Feature Engineering
+df = df[df['Uy'] < 0]
+for i in range(10):
+    df = df[df[f'soil_layer_{i}'] > 20000]
+
+
 X = df.drop(columns=['Uy','rot'])
 y = df['Uy']
 
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.02, random_state=42)
-model = LinearRegression()
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=6)
+#model = LinearRegression()
+#model = Lasso(alpha=0.2)
+model = LassoCV(alphas=[0.001, 0.01, 0.1, 1, 10, 100], cv=5, random_state=1)
+
+# poly = PolynomialFeatures(degree=2)
+# X_train = poly.fit_transform(X_train)
+# X_test = poly.transform(X_test)
+# model = LinearRegression()
+
+
 model.fit(X_train, y_train)
 
 y_pred = model.predict(X_test)
@@ -47,24 +63,22 @@ import numpy as np
 
 # Brug X_test som input til modellen for at få forudsigelserne
 y_pred = model.predict(X_test)
+y_pred_trained = model.predict(X_train)
 
 # Opret en række indekser for plottene (fra 0 til længden af y_test)
 indices = np.arange(len(y_test))
 
 # Lav et bar plot
-plt.figure(figsize=(10, 6))
+fig, (ax1) = plt.subplots(1, 1, figsize=(10, 5))  
 
-# Plot de faktiske Uy-værdier fra testdatasættet (blå)
-plt.bar(indices - 0.2, y_test, width=0.4, label="Actual Uy (Test Data)", color='blue')
-
-# Plot de forudsigte Uy-værdier fra modellen (rød)
-plt.bar(indices + 0.2, y_pred, width=0.4, label="Predicted Uy (Model)", color='red')
-
-# Tilføj labels, titel og legend
-plt.title("Actual Uy vs Predicted Uy for Test Data")
-plt.xlabel("Index")
-plt.ylabel("Uy Values")
-plt.legend()
-
+ax1.plot(y_test*1000,y_pred/y_test,marker='o',linestyle='',color='b')
+#ax1.plot(y_train*1000,y_pred_trained/y_train,marker='o',linestyle='',color='r')
+ax1.invert_xaxis()
+ax1.set_xlabel('Actual settlement [mm]')
+ax1.set_ylabel('Predicted settlement / Actual settlement [-]')
+ax1.minorticks_on()
+ax1.grid(which='major',alpha=0.5)
+ax1.grid(which='minor',alpha=0.2)
+ax1.set_ylim(ymin=0, ymax=5)
 # Vis plottet
 plt.show()
