4a386a01d7c5fe2a9a8b3c33ec401cca0a81d2f3
JM Kirstein <jmk1589@gmail.com>
2025-01-05 10:23:36 +0100

    Improving ML script for settlement interpretation

--- foundationResponse/ML/databaseCreation.py
+++ foundationResponse/ML/databaseCreation.py
@@ -4,30 +4,36 @@ import math
 import json
 
 # Foundation Sizes
-w_min = 1
-w_max = 4
+foundationWidth = [1,1.5,2,2.5,3,3.5,4]
+
 # Soil E module variations
-E_min = 5000
-E_max = 200000
+E_min = 10000
+E_max = 100000
+soilModel = 'linear'
 # Eccentricity variation in relation to foundation with10
 ecc_min = 0
 ecc_max = 0.3
 
-plaxis_password = '12333333344342sdfdf'
-dataFile_path = r'C:\Users\jmkir\Ramboll\JMKIR - Documents\10. projects\94. ML, Rocking foundation\dataFile.json'
-calculationsToRun = 1000
+soilLayerThickness = 0.5
+modelWidthFactor = 4 # Model with in total, in relation to the foundation width
+modelDepthFactor = 2 # Model depth in relation to the foundation width
+
+
+plaxis_password = 'w!V+ZGheHT%V!6U^'
+dataFile_path = r'C:\Users\jmkir\Ramboll\JMKIR - Documents\personalWebpage\foundationResponse\ML\dataFile_test.json'
+calculationsToRun = 500
 
 s_i, g_i = new_server('localhost', 10000, password=plaxis_password)
 
 def createModel(data,plaxis_password):
-	model_width = 4*data['foundationWidth']
-	model_depth = 2*data['foundationWidth']
+	model_width = modelWidthFactor*data['foundationWidth']
+	model_depth = modelDepthFactor*data['foundationWidth']
 
 	s_i.new()
 	g_i.soilContour.initializerectangular(-model_width/2,-model_depth,model_width/2,0)
 	g_i.borehole(0)
 	for E_soil in data['soils']:
-		createLayer(E_soil)
+		createLayer(data['soilModel'],E_soil)
 
 	g_i.gotostructures()
 	g_i.plate((-data['foundationWidth']/2,0),(data['foundationWidth']/2,0))
@@ -53,11 +59,13 @@ def createModel(data,plaxis_password):
 	g_o.close()
 	return Uy_max, Uy_min
 
-def createLayer(E_soil):
+def createLayer(soilModel,E_soil):
 	material = g_i.soilmat()
-	material.setproperties("SoilModel", 2, "gammaUnsat", 20, 
-	"gammaSat", 20, "ERef", E_soil, "nu", 0.3, "phi", 40, "cRef", 300)
-	g_i.soillayer(0.1)
+	if soilModel == 'linear':
+		material.setproperties("SoilModel", 1, "gammaUnsat", 20,"gammaSat", 20, "ERef", E_soil, "nu", 0.3)
+	elif soilModel == 'MC':
+		material.setproperties("SoilModel", 2, "gammaUnsat", 20, "gammaSat", 20, "ERef", E_soil, "nu", 0.3, "phi", 40, "cRef", 300)	
+	g_i.soillayer(soilLayerThickness)
 	g_i.Soils[-1].Material = material
 
 def datafile_readAndAppend(dataFile_path,data):
@@ -71,12 +79,16 @@ def datafile_readAndAppend(dataFile_path,data):
 	with open(dataFile_path, "w") as file:
 		json.dump(dataFile, file, indent=4)
 
-def appendDataFile(dataFile_path, plaxis_password, w_min, w_max, E_min, E_max, ecc_min, ecc_max):
+def appendDataFile(dataFile_path, plaxis_password, foundationWidth, E_min, E_max, ecc_min, ecc_max,soilLayerThickness, soilModel):
 	data = dict()
-	data['foundationWidth'] = float(format(random.uniform(w_min,w_max),'0.1f'))
-	model_depth = 2*data['foundationWidth']
+	data['foundationWidth'] = float(format(random.choice(foundationWidth),'0.1f'))
+	model_depth = modelDepthFactor*data['foundationWidth']
 	data['eccentricity'] = float(format(random.uniform(ecc_min,ecc_max)*data['foundationWidth'],'0.2f'))
-	data['soils'] = [float(format(random.uniform(E_min,E_max),'.0f')) for i in range(int(model_depth*10))]
+	data['soilModel'] = soilModel
+	numberOfLayers = model_depth / soilLayerThickness
+	#soilE = float(format(random.uniform(E_min,E_max),'.0f')) ## This line is used to create a single E soil layer
+	#data['soils'] = [soilE for i in range(int(numberOfLayers))] ## This line is used to create a single E soil layer
+	data['soils'] = [float(format(random.uniform(E_min,E_max),'.0f')) for i in range(int(numberOfLayers))]
 
 	Uy_max, Uy_min = createModel(data,plaxis_password)
 	diffY = Uy_max - Uy_min
@@ -87,5 +99,5 @@ def appendDataFile(dataFile_path, plaxis_password, w_min, w_max, E_min, E_max, e
 
 print('Calculation initiated')
 for i in range(calculationsToRun):
-	appendDataFile(dataFile_path, plaxis_password, w_min, w_max, E_min, E_max, ecc_min, ecc_max)
+	appendDataFile(dataFile_path, plaxis_password, foundationWidth, E_min, E_max, ecc_min, ecc_max,soilLayerThickness,soilModel)
 	print(f'Iteration {i} finalized')
--- foundationResponse/ML/linearRegression.py
+++ foundationResponse/ML/linearRegression.py
@@ -1,18 +1,27 @@
+
 import numpy as np
 import pandas as pd
 from sklearn.linear_model import LinearRegression, Lasso, ElasticNet, LassoCV
-from sklearn.preprocessing import PolynomialFeatures
+from sklearn.preprocessing import PolynomialFeatures, StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
+import matplotlib.pyplot as plt
+from sklearn.ensemble import RandomForestRegressor
 
-filePath = "dataFile.json"
+#pd.set_option('display.max_rows', None)  # Show all rows
+pd.set_option('display.max_columns', None)  # Show all columns
+pd.set_option('display.width', None)  # No line width limit
+pd.set_option('display.max_colwidth', None)  # No limit on column width
+
+
+
+filePath = r"C:\Users\jmkir\Ramboll\JMKIR - Documents\personalWebpage\foundationResponse\ML\dataFile_test - copy.json"
 df = pd.read_json(filePath)
 
 # Substituting no soil values with a hard soil placeholder value
 maxSoilLayers = max([len(i) for i in df['soils']])
 placeholder_soilValue = 10e7
 
-maxSoilLength = 80
 def fillSoilArray(array,maxLength,placeholderValue):
     fillLength = maxLength - len(array)
     array.extend([placeholderValue] * fillLength) 
@@ -20,65 +29,52 @@ def fillSoilArray(array,maxLength,placeholderValue):
 
 df['soilsNew'] = df.apply(lambda row: fillSoilArray(row['soils'], maxLength=maxSoilLayers, placeholderValue = placeholder_soilValue), axis=1)
 
-soils_df = pd.DataFrame(df['soilsNew'].to_list(), columns=[f'soil_layer_{i}' for i in range(80)])
+soils_df = pd.DataFrame(df['soilsNew'].to_list(), columns=[f'soil_layer_{i}' for i in range(maxSoilLayers)])
 df = pd.concat([df, soils_df], axis=1)
-df = df.drop(columns=['soils','soilsNew'])
-
+df = df.drop(columns=['soils','soilsNew','soilModel'])
+#df = df[df['foundationWidth'] > 2]
 # Feature Engineering
 df = df[df['Uy'] < 0]
-for i in range(10):
-    df = df[df[f'soil_layer_{i}'] > 20000]
-
-
+df = df.apply(lambda x: 1/x if x.name.startswith('soil_layer') else x)
 X = df.drop(columns=['Uy','rot'])
 y = df['Uy']
 
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1)
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=6)
-#model = LinearRegression()
-#model = Lasso(alpha=0.2)
-model = LassoCV(alphas=[0.001, 0.01, 0.1, 1, 10, 100], cv=5, random_state=1)
-
-# poly = PolynomialFeatures(degree=2)
-# X_train = poly.fit_transform(X_train)
-# X_test = poly.transform(X_test)
-# model = LinearRegression()
+lin_model = LinearRegression()
+lin_model.fit(X_train_scaled, y_train)
+lin_pred = lin_model.predict(X_test_scaled)
+lin_errors = (lin_pred-y_test)/y_test
 
 
-model.fit(X_train, y_train)
 
-y_pred = model.predict(X_test)
+rf_model = RandomForestRegressor(n_estimators=100)
+rf_model.fit(X_train, y_train)
+rf_pred = rf_model.predict(X_test)
+rf_errors = (rf_pred-y_test)/y_test
 
-# Evaluer modellen
-mse = mean_squared_error(y_test, y_pred)
-r2 = r2_score(y_test, y_pred)
-mae = mean_absolute_error(y_test, y_pred)
+# Exploring max errors
+errors = pd.DataFrame({'error':rf_errors})
+df = pd.concat([1/X_test,errors],axis=1)
 
-print(f"Mean Absolute Error: {mae}")
-print(f'Mean Squared Error (MSE): {mse}')
-print(f'R^2 Score: {r2}')
 
-import matplotlib.pyplot as plt
-import numpy as np
+# Plotting
+fig, (ax1) = plt.subplots(1, 1, figsize=(10, 5))
 
-# Brug X_test som input til modellen for at få forudsigelserne
-y_pred = model.predict(X_test)
-y_pred_trained = model.predict(X_train)
-
-# Opret en række indekser for plottene (fra 0 til længden af y_test)
-indices = np.arange(len(y_test))
-
-# Lav et bar plot
-fig, (ax1) = plt.subplots(1, 1, figsize=(10, 5))  
-
-ax1.plot(y_test*1000,y_pred/y_test,marker='o',linestyle='',color='b')
-#ax1.plot(y_train*1000,y_pred_trained/y_train,marker='o',linestyle='',color='r')
+ax1.plot(y_test*1000,lin_errors,marker='o',linestyle='',color='b',label='Linear Regression')
+ax1.plot(y_test*1000,rf_errors,marker='o',linestyle='',color='r',label='Random Forest Regression')
 ax1.invert_xaxis()
 ax1.set_xlabel('Actual settlement [mm]')
 ax1.set_ylabel('Predicted settlement / Actual settlement [-]')
 ax1.minorticks_on()
 ax1.grid(which='major',alpha=0.5)
 ax1.grid(which='minor',alpha=0.2)
-ax1.set_ylim(ymin=0, ymax=5)
+ax1.legend()
+#ax1.set_ylim(ymin=-1, ymax=5)
 # Vis plottet
 plt.show()
+
+print(df.iloc[:,2:].sort_values('error',ascending=False))
