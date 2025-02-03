0badc129ab6cbb361a41ad24e3884251dfdcee36
Krusovice <jmk1589@gmail.com>
2025-02-03 20:25:24 +0100

    dockerized webpage and airflow + database to postgres

---
+++ Dockerfile
@@ -0,0 +1,26 @@
+# Use an official Python runtime as a parent image
+FROM python:3.11.2
+
+# Set the working directory inside the container to /personalWebpage/personalWebpage
+WORKDIR /personalWebpage/personalWebpage
+
+# Install system dependencies
+RUN apt-get update && apt-get install -y \
+    build-essential \
+    libpq-dev \
+    && rm -rf /var/lib/apt/lists/*
+
+# Copy the current directory contents into the container at /personalWebpage/personalWebpage
+COPY . /personalWebpage/
+
+# Install any needed packages specified in requirements.txt
+RUN pip install --no-cache-dir -r /personalWebpage/requirements.txt
+
+# Expose the port that Gunicorn will run on
+EXPOSE 8000
+
+# Define environment variable
+ENV PYTHONUNBUFFERED 1
+
+# Run Gunicorn as the command
+CMD ["gunicorn", "personalWebpage.wsgi:application", "--bind", "0.0.0.0:8000"]
--- personalWebpage/personalWebpage/settings.py
+++ personalWebpage/personalWebpage/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+import psycopg2
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,14 +78,28 @@ TEMPLATES = [
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'personalWebpage_db',
+        'USER': 'Krusovice',
+        'PASSWORD': 'fedefrede',
+        'HOST': 'localhost',  # Or the IP address of your container
+        'PORT': '5432',
     }
 }
-
+'''
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB', 'personalWebpage_db'),
+        'USER': os.getenv('POSTGRES_USER', 'Krusovice'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'fedefrede'),
+        'HOST': os.getenv('POSTGRES_HOST', 'postgres'),
+        'PORT': '5432',
+    }
+}
 
 
 # Password validation
