3cd7dc26863bdfa5ebf5d4977ee13385db921215
Krusovice <jmk1589@gmail.com>
2025-02-13 23:32:18 +0100

    astro airflow course

--- astro-airflow/dags/stock_market.py
+++ astro-airflow/dags/stock_market.py
@@ -5,7 +5,7 @@ from airflow.hooks.base import BaseHook
 from airflow.operators.python import PythonOperator
 from airflow.providers.docker.operators.docker import DockerOperator
 
-from include.stock_market.tasks import _get_stock_prices, _store_prices
+from include.stock_market.tasks import _get_stock_prices, _store_prices, _get_formatted_csv
 
 SYMBOL = 'NVDA'
 
@@ -59,7 +59,15 @@ def stock_market():
 		}
 	)
 
+	get_formatted_csv = PythonOperator(
+		task_id='get_formatted_csv',
+		python_callable=_get_formatted_csv,
+		op_kwargs={
+			'path': '{{ ti.xcom_pull(task_ids="store_prices") }}'
+		}
 
-	is_api_available() >> get_stock_prices >> store_prices >> format_prices
+	)
+
+	is_api_available() >> get_stock_prices >> store_prices >> format_prices >> get_formatted_csv
 
 stock_market()
